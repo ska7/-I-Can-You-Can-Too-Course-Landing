861f151a433875b84ce963f5ebb8fe1146c2c77e
DESKTOP-1B4SU0S\Nikita <nikkofann@gmail.com>
2020-12-13 12:00:57 +0200

    fixed video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Iframe from "react-iframe";
 import YouTube from "react-youtube";
 
 export default function Video({ url }) {
+  const [opts, setOpts] = useState({
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  });
+
+  const handlePlay = (event) => {
+    event.target.setSize(window.screen.width, window.screen.height);
+  };
+
+  const handleStop = (event) => {
+    event.target.setSize("100%", "100%");
+  };
+
   return (
     <div style={blockStyle} className="video-section">
       <h1 style={h1Style}>ВИДЕООБРАЩЕНИЕ ОТ НАШЕЙ ЖЕНСКОЙ КОМАНДЫ</h1>
-      {/* <div style={iframeStyle}> */}
-      {/* <Iframe
-          url="http://www.youtube.com/embed/DZuXhbpuRRY"
-          width="90%"
-          height="auto"
-        /> */}
-      <YouTube videoId="DZuXhbpuRRY" opts={opts} />
-      {/* </div> */}
+      <div style={iframeStyle}>
+        <YouTube
+          videoId="DZuXhbpuRRY"
+          id="player"
+          opts={opts}
+          onPlay={handlePlay}
+          onPause={handleStop}
+          onEnd={handleStop}
+        />
+      </div>
     </div>
   );
 }
@@ -35,13 +53,12 @@ const h1Style = {
   fontSize: "30px",
 };
 
-const opts = {
-  // Youtube embed styling
+const iframeStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "200px",
   width: "90%",
-  height: "auto",
-};
-
-const youtubeStyle = {
-  maxWidth: "480px",
-  height: "600px",
+  background: "rgb(98,110,96)",
 };

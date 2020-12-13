905e7ff22fad90a84eb3eea2ada4aa23a9e1e490
DESKTOP-1B4SU0S\Nikita <nikkofann@gmail.com>
2020-12-13 10:37:34 +0200

    heroku test

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,16 +5,20 @@ export default function Video({ url }) {
   return (
     <div style={blockStyle} className="video-section">
       <h1 style={h1Style}>ВИДЕООБРАЩЕНИЕ ОТ НАШЕЙ ЖЕНСКОЙ КОМАНДЫ</h1>
-      <Iframe
-        url="http://www.youtube.com/embed/xDMP3i36naA"
-        styles={youtubeStyle}
-      />
+      <div style={iframeStyle}>
+        <Iframe
+          url="http://www.youtube.com/embed/xDMP3i36naA"
+          styles={youtubeStyle}
+          width="90%"
+          height="auto"
+        />
+      </div>
     </div>
   );
 }
 
 const blockStyle = {
-  //   background: "rgb(225,198,153)",
+  background: "rgb(225,198,153)",
   height: "500px",
   display: "flex",
   flexDirection: "column",
@@ -29,7 +33,17 @@ const h1Style = {
   fontSize: "30px",
 };
 
+const iframeStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "200px",
+  width: "90%",
+  background: "rgb(98,110,96)",
+};
+
 const youtubeStyle = {
   maxWidth: "480px",
-  height: "440px",
+  height: "600px",
 };
